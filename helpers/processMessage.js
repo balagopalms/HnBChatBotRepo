1a98b5e6d54aed84fd2d61a6b052ec7f1d45b498
dk4software <dk4software@gmail.com>
2018-09-02 22:08:57 +0530

    modified

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -1,32 +1,8 @@
-const API_AI_TOKEN = 'ec4a634f004248bdb08bd20e0f0a8d4a';
+const API_AI_TOKEN = 'd6727836ebd54822894b04fd2518d18f';
+
 const apiAiClient = require('apiai')(API_AI_TOKEN);
+const intentHandler = require('./intentHandler');
 
-const FACEBOOK_ACCESS_TOKEN = 'EAAJpZCJzwtZAMBAARzK0S1BG1hpSEZCziqbcNDybgictya2tYNuhhe9ZB2GQd0gZBFECOdsnC4q75DlGAIj059d6EVRWEjE5Afeiu2kxg8NmHdawkTqE5xQ9BZB32zcBCdelwKqg3eELfuwqPm6c7AQh8SRAqFZCZCVJLxT8unY5xiDKVsQVwKyBKbdUKlLNveYZD';
-const request = require('request');
-
-const sendTextMessage = (senderId, text) => {
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: FACEBOOK_ACCESS_TOKEN },
-        method: 'POST',
-        body: {
-            recipient: { id: senderId },
-            message: { text },
-        },
-        json: true
-    });
-};
-
-const callCommerce = (emailId) => {
-    request({
-        uri: 'https://88289eab.ngrok.io/changepwd',
-        method: 'POST',
-        body: {
-            email: { emailId }
-        },
-        json: true
-    })
-};
 module.exports = (event) => {
     const senderId = event.sender.id;
     console.log("SenderId: " + senderId);
@@ -39,13 +15,7 @@ module.exports = (event) => {
         const result = response.result.fulfillment.speech;
         console.log(response.result.metadata.intentName);
         console.log(response.result.parameters.email);
-        if( response.result.metadata.intentName && 
-                response.result.metadata.intentName == 'PASSWORD_RESET - emailId'
-                && response.result.parameters.email ) {
-            callCommerce(response.result.parameters.email);
-            console.log("Called change password service");
-        }
-        sendTextMessage(senderId, result);
+        intentHandler(senderId, result);
     });
     apiaiSession.on('error', (error) => {
         console.log("Error while connecting to DialogFlow");
