ac1b8245b7adf8a7c593ff14d6694dd19bf5fccc
dk4software <dk4software@gmail.com>
2018-09-04 17:30:18 +0530

    added page with link for facebook chat bot

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 const verificationController = require('./controllers/verification');
 const messageWebhookController = require('./controllers/messageWebhook');
+const indexPage = require('./html/staticPage');
+
 const changePasswordController = require('./controllers/changePassword');
 const dialogFlowTestController = require('./controllers/dialogFlowTest');
 
 app.get('/', verificationController);
 app.post('/', messageWebhookController);
+app.get('/index.html', indexPage);
+
 app.post('/changepwd', changePasswordController);
 app.get('/dialogflowtest', dialogFlowTestController);
 
